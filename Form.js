143d99d5990d003d6685920bb596dbd8f5ce4f81
Fig747 <84551394+Fig747@users.noreply.github.com>
2021-07-07 14:02:25 -1000

    Add files via upload

--- Form.js
+++ Form.js
@@ -3,14 +3,28 @@
 var inputBTN = document.getElementById("SubmitBtn");
 inputBTN.addEventListener('click', myfunction);
 
-function myfunction(e){
-    e.preventDefault();
+function myfunction(){
+    //e.preventDefault();
     var x = document.getElementById("input name");
-    var text = "";
+    var text = [];
     var i;
-    for (i = 0; i < x.length ;i++) {
-      text += x.elements[i].value + "\n";
+    for (i = 0; i < x.length -1 ;i++) {
+      text.push(x.elements[i].value);
     }
-    console.log(text);
-    //document.getElementById("SubmitBtn").innerHTML = text;
+    
+    console.log(func2(text[1], text[2])); // <= call functions to do input
+    //return text;
+
 }
+
+
+
+function func2(num1, num2){
+    totalscore = num1 += num2;
+    return totalscore;
+
+}
+
+
+    //console.log(text);
+    //document.getElementById("SubmitBtn").innerHTML = text;

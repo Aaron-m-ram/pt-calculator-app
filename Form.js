0838f765220eea30322bb2d2be86b77af159d25a
Fig747 <84551394+Fig747@users.noreply.github.com>
2021-07-07 15:25:44 -1000

    Gender selector
    got inputs working and gender dropdown selector

--- Form.js
+++ Form.js
@@ -11,18 +11,37 @@ function myfunction(){
     for (i = 0; i < x.length -1 ;i++) {
       text.push(x.elements[i].value);
     }
-    
-    console.log(func2(text[1], text[2])); // <= call functions to do input
+    var newNum = parseInt(text[1]);
+    var newNum2 = parseInt(text[2]);
+    //console.log(typeof(newNum), typeof(newNum2));
+    //console.log(func2(newNum, newNum2)); // <= call functions to do input
     //return text;
 
+    var inpobj = document.getElementById("inlineFormCustomSelect");
+    strUser = inpobj.value;
+    console.log(typeof strUser);
+
+    if(strUser == 1) { 
+        girl();
+    } else if(strUser == 2) {
+        boy();
+    }
+    else {
+        return null;
+    }
 }
 
-
+function boy() {
+    console.log('it is a boy')
+    
+}
+function girl() {
+    console.log('it is a girl')
+}
 
 function func2(num1, num2){
     totalscore = num1 += num2;
     return totalscore;
-
 }
 
 

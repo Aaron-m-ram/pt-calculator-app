5a7a88e138e71c693123dec17cf5f5ecee5a9c28
GRID Lua Pele <gridluapele@GRIDs-MacBook-Pro.local>
2021-07-08 16:06:58 -1000

    added tests, created coverage report

--- tests.test.js
+++ tests.test.js
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "@jest/globals";
-import { pushUpsGetter } from "./Running";
+import { pushUpsGetter, SitUpsGetter } from "./Running";
 
 describe("PushUpsGetter", () => {
   it("returns 1 point when given 30 pushups", () => {
@@ -24,3 +24,38 @@ describe("PushUpsGetter", () => {
     expect(pushUpsGetter(undefined)).toBe(0);
   });
 });
+describe("SitUpsGetter", () => {
+  it("returns 0 point when given < 39 situps", () => {
+    expect(SitUpsGetter(38, false)).toBe(0);
+  });
+  it("returns 6 points when given 40 situps", () => {
+    expect(SitUpsGetter(40, false)).toBe(6);
+  });
+  it("returns 20 points when given 58 situps", () => {
+    expect(SitUpsGetter(58, false)).toBe(20);
+  });
+  it("returns 20 points when given > 58 situps", () => {
+    expect(SitUpsGetter(59, false)).toBe(20);
+  });
+  it("returns 0 points when given true waiver", () => {
+    expect(SitUpsGetter(0, true)).toBe(0);
+  });
+  it("returns 0 points when given null situps", () => {
+    expect(SitUpsGetter(null, false)).toBe(0);
+  });
+  it("returns 0 points when given undefined situps", () => {
+    expect(SitUpsGetter(undefined, false)).toBe(0);
+  });
+  it("returns 0 points when given undefined waiver", () => {
+    expect(SitUpsGetter(0, undefined)).toBe(0);
+  });
+  it("returns 0 points when given both undefined inputs", () => {
+    expect(SitUpsGetter(undefined, undefined)).toBe(0);
+  });
+});
+
+// describe("RunningGetter", () => {
+//   it("returns 0 when given a score above max time", () => {
+
+//   })
+// });

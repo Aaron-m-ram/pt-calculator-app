a7dcbbc641a6d365246c1c0be051e09c60656043
GRID Lua Pele <gridluapele@GRIDs-MacBook-Pro.local>
2021-07-08 15:22:48 -1000

    installed prettier, and eslint.

--- Running.js
+++ Running.js
@@ -1,44 +1,50 @@
+function GenderGetter(Gender) {
+  if (Gender === true) {
+    return "Male";
+  }
+  if (Gender === false) {
+    return "Female";
+  }
+}
 // let Age = AgeGetter;
 // let Gender = GenderGetter;
 // let PushUps = PushUpsGetter;
 // let SitUps = SitUpsGetter;
 // let RunTime = RunTimeGetter;
-
 function AgeGetter(Age) {
   if (Age < 25) {
     return "17_24";
-  } else if (Age <= 29 && Age >= 25) {
+  }
+  if (Age <= 29 && Age >= 25) {
     return "25_29";
-  } else if (Age <= 34 && Age >= 30) {
+  }
+  if (Age <= 34 && Age >= 30) {
     return "30_34";
-  } else if (Age <= 39 && Age >= 35) {
+  }
+  if (Age <= 39 && Age >= 35) {
     return "35_39";
-  } else if (Age <= 44 && Age >= 40) {
+  }
+  if (Age <= 44 && Age >= 40) {
     return "40_44";
-  } else if (Age <= 49 && Age >= 45) {
+  }
+  if (Age <= 49 && Age >= 45) {
     return "45_49";
-  } else if (Age <= 54 && Age >= 50) {
+  }
+  if (Age <= 54 && Age >= 50) {
     return "50_54";
-  } else if (Age <= 59 && Age >= 55) {
+  }
+  if (Age <= 59 && Age >= 55) {
     return "55_59";
-  } else if (Age >= 60) {
+  }
+  if (Age >= 60) {
     return "60+";
   }
 }
 
-function GenderGetter(Gender) {
-  if (Gender === true) {
-    return "Male";
-  } else if (Gender === false) {
-    return "Female";
-  }
-}
-
-const PushUpsGetter = (pushUpsInput, pushUpWaiver) => {
+const pushUpsGetter = (pushUpsInput, pushUpWaiver) => {
   let pushUpsScore;
-  pushUpWaiver ? (pushUpsScore = 0) : null;
 
-  let pushUpCountToScore = [
+  const pushUpCountToScore = [
     { count: 30, score: 1 },
     { count: 31, score: 4.0 },
     { count: 32, score: 7.0 },
@@ -80,7 +86,12 @@ const PushUpsGetter = (pushUpsInput, pushUpWaiver) => {
   ];
 
   // if pushup input is less than 30, or undefined/null, return 0
-  if (pushUpsInput < 30 || pushUpsInput == undefined || pushUpsInput == null) {
+  if (
+    pushUpsInput < 30 ||
+    pushUpsInput === undefined ||
+    pushUpsInput == null ||
+    pushUpWaiver
+  ) {
     pushUpsScore = 0;
   } else if (pushUpsInput >= 68) {
     // if the pushup input is more than max pushup input, then return max score.
@@ -89,122 +100,129 @@ const PushUpsGetter = (pushUpsInput, pushUpWaiver) => {
     // declaritive programming vs imperitive programming ---
     // search for the first instance where the object {count:number score: number}
     // has the same count as pushupsInput
-    pushUpsScore = pushUpCountToScore.find((scoringObject) => {
-      return scoringObject.count == pushUpsInput;
-    }).score;
+    pushUpsScore = pushUpCountToScore.find(
+      (obj) => obj.count === pushUpsInput
+    ).score;
   }
   return pushUpsScore;
 };
 
 function SitUpsGetter(SitUps, SitUpWaiver) {
-  if (SitUpWaiver == true) {
-    console.log("Sit up Waiver");
+  if (SitUpWaiver) {
     return 0.0;
-  } else if (SitUps < 39) {
-    console.log("Auto Fail Situps ");
+  }
+  if (SitUps < 39) {
     return 0.0;
-  } else if (SitUps == 39) {
+  }
+  if (SitUps === 39) {
     return 3.0;
-  } else if (SitUps == 40) {
+  }
+  if (SitUps === 40) {
     return 6.0;
-  } else if (SitUps == 41) {
+  }
+  if (SitUps === 41) {
     return 9.0;
-  } else if (SitUps == 42) {
+  }
+  if (SitUps === 42) {
     return 12.0;
-  } else if (SitUps == 43) {
+  }
+  if (SitUps === 43) {
     return 12.6;
-  } else if (SitUps == 44) {
+  }
+  if (SitUps === 44) {
     return 13.0;
-  } else if (SitUps == 45) {
+  }
+  if (SitUps === 45) {
     return 14.0;
-  } else if (SitUps == 46) {
+  }
+  if (SitUps === 46) {
     return 15.0;
-  } else if (SitUps == 47) {
+  }
+  if (SitUps === 47) {
     return 16.0;
-  } else if (SitUps == 48) {
+  }
+  if (SitUps === 48) {
     return 16.6;
-  } else if (SitUps == 49) {
+  }
+  if (SitUps === 49) {
     return 17.0;
-  } else if (SitUps == 50) {
+  }
+  if (SitUps === 50) {
     return 17.4;
-  } else if (SitUps == 51) {
+  }
+  if (SitUps === 51) {
     return 17.6;
-  } else if (SitUps == 52) {
+  }
+  if (SitUps === 52) {
     return 18.0;
-  } else if (SitUps == 53) {
+  }
+  if (SitUps === 53) {
     return 18.4;
-  } else if (SitUps == 54) {
+  }
+  if (SitUps === 54) {
     return 18.8;
-  } else if (SitUps == 55) {
+  }
+  if (SitUps === 55) {
     return 19.0;
-  } else if (SitUps == 56) {
+  }
+  if (SitUps === 56) {
     return 19.4;
-  } else if (SitUps == 57) {
+  }
+  if (SitUps === 57) {
     return 19.7;
-  } else if (SitUps >= 58) {
+  }
+  if (SitUps >= 58) {
     return 20.0;
   }
 }
 
 function RunTimeGetter(RunTime, RunWaiver) {
-  if (RunWaiver == true) {
-    console.log("Run Waiver");
+  if (RunWaiver) {
     return 0.0;
-  } else if (RunTime > "1551") {
-    console.log("Auto Fail Run ");
-  } else if (RunTime == "0912") {
+  }
+  if (RunTime > "1551") {
+    return 0;
+  }
+  if (RunTime === "0912") {
     return 60.0;
   }
 }
 
 function FinalScore(
-  PushUpsGetter,
-  PushUpWaiver,
-  SitUpsGetter,
-  SitUpWaiver,
-  RunTimeGetter,
-  RunWaiver
+  handlePushUpsGetter,
+  handlePushUpWaiver,
+  handleSitUpsGetter,
+  handleSitUpWaiver,
+  handleRunTimeGetter,
+  handleRunWaiver
 ) {
-  var TotalScore = PushUpsGetter + SitUpsGetter + RunTimeGetter;
-  if (SitUpWaiver == true && PushUpWaiver == false && RunWaiver == false) {
+  let TotalScore =
+    handlePushUpsGetter + handleSitUpsGetter + handleRunTimeGetter;
+  if (handleSitUpWaiver && !handlePushUpWaiver && !handleRunWaiver) {
     TotalScore = (TotalScore / 80) * 100;
     return TotalScore;
-  } else if (
-    SitUpWaiver == true &&
-    PushUpWaiver == true &&
-    RunWaiver == false
-  ) {
+  }
+  if (handleSitUpWaiver && handlePushUpWaiver && !handleRunWaiver) {
     TotalScore = (TotalScore / 60) * 100;
     return TotalScore;
-  } else if (SitUpWaiver == true && PushUpWaiver == true && RunWaiver == true) {
+  }
+  if (handleSitUpWaiver && handlePushUpWaiver && handleRunWaiver) {
     TotalScore = (TotalScore / 0) * 100;
     return TotalScore;
-  } else if (
-    SitUpWaiver == true &&
-    PushUpWaiver == false &&
-    RunWaiver == true
-  ) {
+  }
+  if (handleSitUpWaiver && !handlePushUpWaiver && handleRunWaiver) {
     TotalScore = (TotalScore / 20) * 100;
     return TotalScore;
-  } else if (
-    SitUpWaiver == false &&
-    PushUpWaiver == true &&
-    RunWaiver == false
-  ) {
+  }
+  if (handlePushUpWaiver && !handleSitUpWaiver && !handleRunWaiver) {
     TotalScore = (TotalScore / 80) * 100;
     return TotalScore;
-  } else if (
-    SitUpWaiver == false &&
-    PushUpWaiver == true &&
-    RunWaiver == true
-  ) {
+  }
+  if (!handleSitUpWaiver && handlePushUpWaiver && handleRunWaiver) {
     TotalScore = (TotalScore / 20) * 100;
     return TotalScore;
-  } else if (
-    SitUpWaiver == false &&
-    PushUpWaiver == false &&
-    RunWaiver == true
-  ) {
+  }
+  if (!handleSitUpWaiver && !handlePushUpWaiver && handleRunWaiver) {
     TotalScore = (TotalScore / 40) * 100;
     return TotalScore;
   }
@@ -227,7 +245,7 @@ function FinalScore(
 export {
   AgeGetter,
   GenderGetter,
-  PushUpsGetter,
+  pushUpsGetter,
   SitUpsGetter,
   RunTimeGetter,
   FinalScore,
--- tests.test.js
+++ tests.test.js
@@ -1,25 +1,26 @@
-import { PushUpsGetter } from "./Running";
+import { describe, expect, it } from "@jest/globals";
+import { pushUpsGetter } from "./Running";
 
 describe("PushUpsGetter", () => {
   it("returns 1 point when given 30 pushups", () => {
-    expect(PushUpsGetter(30)).toBe(1);
+    expect(pushUpsGetter(30)).toBe(1);
   });
   it("returns 20 points when given 67 pushups", () => {
-    expect(PushUpsGetter(67)).toBe(20);
+    expect(pushUpsGetter(67)).toBe(20);
   });
   it("returns 0 points when given < 30 pushups", () => {
-    expect(PushUpsGetter(29)).toBe(0);
+    expect(pushUpsGetter(29)).toBe(0);
   });
   it("returns 20 points when given > 67 pushups", () => {
-    expect(PushUpsGetter(68)).toBe(20);
+    expect(pushUpsGetter(68)).toBe(20);
   });
   it("returns 0 points when given 0 pushups", () => {
-    expect(PushUpsGetter(0)).toBe(0);
+    expect(pushUpsGetter(0)).toBe(0);
   });
   it("returns 0 points when given null pushups", () => {
-    expect(PushUpsGetter(null)).toBe(0);
+    expect(pushUpsGetter(null)).toBe(0);
   });
   it("returns 0 points when given undefined pushups", () => {
-    expect(PushUpsGetter(undefined)).toBe(0);
+    expect(pushUpsGetter(undefined)).toBe(0);
   });
 });
